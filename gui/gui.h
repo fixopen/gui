1daa036438a4885bff196ef04d8fd3b80de6e373
dup <fixopen@gmail.com>
2020-10-13 12:18:02 +0800

    append control files

--- gui/gui.h
+++ gui/gui.h
@@ -6,3 +6,19 @@
 #include <iostream>
 
 // TODO: Reference additional headers your program requires here.
+// use coroutine for event 机制
+// use C++ module, concept, ranges, and container
+// use react-native, flutter, swiftUI, winUI and composition 模式
+// support MVVM
+// cross Windows, Linux, macOS, Android, iOS
+
+// DisplayUtil: drawImage(image, position, size, layout), drawText(text, position, size, font, layout)
+// Control: size, draw[color, image, font(text layout), DC], click[select], input[keyboard], cursorOver[mouse, point, touch], hide, show, disable, enable, resize
+// Container: children(position) findControl, layout(absolute, border, flow, flex, list, grid, tile, tab, frame)
+// Scrollable: viewPort scrollTo(position)
+// Tree: shrink expand
+// RichEditor, Editor
+// DateTime Color File Progress Slider Check Select Menu ContextMenu Image Flash Video Screen Audio Speaker Recorder Recognizer TouchPanel Gesture Camera
+// Presenter + Data(Multimedia) Codec
+// WebView
+// ActiveX Combo DateTime Edit IPAddress
